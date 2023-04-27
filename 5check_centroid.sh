c32328ddf72f2f9cabe0129955606c299093a438
ccsmm78 <ccsmm78@gmail.com>
2023-04-27 15:32:19 +0900

    Moved code url to DeepGuider homepage.

--- 5check_centroid.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-## Run your virtual environment activation script
-source ~/.virtualenvs/dg_venv3.6/bin/activate
-source scripts/common.bash
-
-check_centroid
--- networks/MobileNet/__init__.py
+++
@@ -1,3 +0,0 @@
-from .modeling import *
-from ._deeplab import convert_to_separable_conv
-from .cityscapes import * 
